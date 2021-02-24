d4b863267d6298f11d8a5ac30178e857096a578a
Aya <ayamallah@hotmail.co.uk>
2021-02-24 15:16:34 +0000

    Added time/date

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let days = ["Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    return `${day}, ${hours}:${minutes}`;
+}
+
+
 function displayWeather(response) {
     console.log(response.data)
 
@@ -15,6 +35,9 @@ function displayWeather(response) {
 
     let wind = document.querySelector("#wind");
     wind.innerHTML = Math.round(response.data.wind.speed);
+
+    let date = document.querySelector("#date");
+    date.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "4c4b730fc952d2218d8c25bb1938764c";
